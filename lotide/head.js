b4f67fe6c696f601eff365a16587712cfcb9bdd3
JeongminJamie <sksrhcalsu@gmail.com>
2022-05-03 08:38:15 -0400

    head

--- lotide/head.js
+++ lotide/head.js
@@ -1,12 +1,17 @@
 const head = function(array) {
-    return array[0];
-    }
-    
-  const assertEqual = function(actual, expected) {
-    actual = head(array);
-    expected = head[0];
-  };
-  
+  console.log(array[0]);
+}; 
+
+
+const assertEqual = function(actual, expected) {
+  actual = head;
+  if (actual[0] === expected) {
+    console.log(expected);
+  } else if (actual[0] === []) {
+    console.log("undefined");
+  }
+};
+
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-
+assertEqual(head([]), 5);
