df74cba9a596bd056d516895c5c9eddfb9b25e44
JeongminJamie <sksrhcalsu@gmail.com>
2022-05-06 10:31:15 -0400

    assert object equal

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,3 +1,21 @@
+const eqArrays = function(array1, array2) {
+  let result;
+  if (array1.length !== array2.length) {
+    result = false;
+    return result;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        result = false;
+        return result;
+      } else {
+        result = true;
+      }
+    }
+    return result;
+  }
+};
+
 const eqObjects = function (object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
@@ -20,9 +38,15 @@ const eqObjects = function (object1, object2) {
     return returnValue;
   };
 
-const assertObjectsEqual = function(actual, expected) {
-  const inspect = require('util').inspect;
-  if (actual === expected) {
-    
-  }
-};
+  const assertObjectsEqual = function(actual, expected) {
+    const inspect = require('util').inspect;  
+    let final = eqObjects(actual, expected);
+
+    if (final === true) {
+      return `❤️❤️❤️ Assertion passed: ${inspect(actual)} === ${inspect(expected)}`;
+    } else {
+      return `😅😅😅 Assertion failed: ${inspect(actual)} !== ${inspect(expected)}`;
+    }
+  };
+
+ console.log(assertObjectsEqual({ a: '1', b: 2 }, { b: 2, a: '1' }));
