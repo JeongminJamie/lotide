29310e3e51023583a538a50227a6f6860655041d
JeongminJamie <sksrhcalsu@gmail.com>
2022-05-09 17:33:44 -0400

    mocha chai test tail

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,23 @@
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
+// const tail = require('../tail');
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// const first = ["hello"];
+// tail(words);
+// tail(first);
+// assertEqual(words.length, 3);
+
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-const first = ["hello"];
-tail(words);
-tail(first);
-assertEqual(words.length, 3);
+describe('#tail', () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Lighthouse', 'Labs']", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    assert.deepEqual(tail(words), ["Lighthouse", "Labs"]);
+  });
+  
+  it("returns [] for []", () => {
+    const first = ["hello"];
+    assert.deepEqual(tail(first), []);
+  });
+});
