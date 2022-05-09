cd8ed7cfd1b042ccb64888486f1a1f7ba891897b
JeongminJamie <sksrhcalsu@gmail.com>
2022-05-09 16:36:47 -0400

    test week2 day1

--- middle.js
+++ middle.js
@@ -1,33 +1,3 @@
-const eqArrays = function(first, second) {
-  let min;
-  if (first.length !== second.length) {
-    result = false;
-    return result; 
-  } else {
-    for (let i = 0; i < first.length; i++) {
-      if (first[i] !== second[i]) {
-        result = false;
-        return result;
-      } else {
-        return true;
-      }
-    }
-    return result;
-  }
-  };
-
-const assertArraysEqual = function(array3, array4) {
-  let result2 = eqArrays(array3,array4);
-  if (result2 === true) {
-    console.log(`❤️❤️❤️: ${array3} and ${array4} are equal ❤️❤️❤️`);
-    return result2;
-  } else {
-    console.log(`😭😭😭 ${array3} and ${array4} are not equal 😭😭😭`);
-    return result2;
-  }
-};
-
-
 const middle = function(array) {
   if (array.length <= 2) {
     return [];
@@ -40,9 +10,5 @@ const middle = function(array) {
   }
 };
 
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
 
-assertArraysEqual(middle([1, 2, 3]), true);
+module.exports = middle;
