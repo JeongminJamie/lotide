e046e7fcecffa5542e48ad0e45008710589913e5
Jeongmin Choi <103073389+JeongminJamie@users.noreply.github.com>
2022-05-09 10:21:22 -0400

    Update findKey.js

--- findKey.js
+++ findKey.js
@@ -1,6 +1,25 @@
+
+   const eqArrays = function(array1, array2) {
+  let result;
+  if (array1.length !== array2.length) {
+    result = false;
+    return result;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        result = false;
+        return result;
+      } else {
+        result = true;
+      }
+    }
+    return result;
+  }
+};
+
 const assertArraysEqual = function(array3, array4) {
   let result2 = eqArrays(array3,array4);
-  if (result === true) {
+  if (result2 === true) {
     console.log(`❤️❤️❤️: ${array3} and ${array4} are equal ❤️❤️❤️`);
     return result2;
   } else {
