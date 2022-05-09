bdffcf25e3d2fe45e4f5c30167e8ab49be212220
JeongminJamie <sksrhcalsu@gmail.com>
2022-05-09 14:54:29 -0400

    assertEqual and head test

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,4 @@
-var emoji = require("node-emoji");
-const heart = emoji.get("heart");
-const sad = emoji.get("x");
-
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(heart + heart + heart + "Assertion passed: " + actual + " === " + expected);
   } else {
@@ -10,6 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
+module.exports = assertEqual;
